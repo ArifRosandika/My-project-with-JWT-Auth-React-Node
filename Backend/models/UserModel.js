cb989bdcb63922ecf064cde0bb857b7c6f168766
Arif Rosandika <arifrosandika@gmail.com>
2025-08-24 14:57:19 +0700

    UserModel.js

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,34 +1,3 @@
-// import { Sequelize } from "sequelize";
-// import db from "../config/Database.js";
-
-// const {DataTypes} = Sequelize;
-
-// const Users = db.define('users', {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     username: {
-//         type: DataTypes.STRING,
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//     },
-//     password: {
-//         type: DataTypes.STRING,
-//     },
-//     refresh_token: {
-//         type: DataTypes.TEXT,
-//         allowNull: true
-//     }
-// }, {
-//     freezeTableName: true,
-//     timestamps: true
-// });
-
-// export default Users; 
-
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 
